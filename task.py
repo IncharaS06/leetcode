55127a4caf53aed9a1daa80348609adbf9703bc2
Inchara S <130211628+IncharaS06@users.noreply.github.com>
2025-09-14 22:31:11 +0530

    Update task.py

--- task.py
+++ task.py
@@ -4,3 +4,16 @@ class Solution:
         for index,i in enumerate(s):
             output+=(123-ord(i))*(index+1)
         return output
+
+
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        count = 0
+        seen = False
+
+        for i in s:
+            if i == '*' and seen == False:
+                count += 1
+            if i == '|':
+                seen = not seen 
+        return count
