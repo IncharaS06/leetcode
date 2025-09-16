632f0a8170bf7d40d8f6c09d8bca91d156d3bfbf
Inchara S <130211628+IncharaS06@users.noreply.github.com>
2025-09-16 23:15:07 +0530

    Update task.py

--- task.py
+++ task.py
@@ -32,3 +32,18 @@ class Solution:
 
             if i == 0:
                 return [1] + digits
+
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        countL = 0
+        countR = 0
+        result = 0
+        for i in s:
+            if i == 'L':
+                countL += 1
+            else:
+                countR += 1
+            if countR == countL:
+                result += 1
+        return result      
