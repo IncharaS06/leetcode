57ff599c4cc577f0918ae634b2ef1098ab820455
Inchara S <130211628+IncharaS06@users.noreply.github.com>
2025-09-17 23:22:25 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -12,3 +12,17 @@ class Solution:
 
         postorder(root)
         return res
+
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        heap=[(x, i) for i, x in enumerate(nums)]
+        heapify(heap)
+        for _ in range(k):
+            x, i=heap[0]
+            heapreplace(heap, (x*multiplier, i))
+        for x, i in heap:
+            nums[i]=x
+        return nums
+
+
+        
