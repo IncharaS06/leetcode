c9f7559d21e35eb972bb39f7e2e66e1120ef51c1
Inchara S <130211628+IncharaS06@users.noreply.github.com>
2025-09-19 22:46:59 +0530

    Update task.py

--- task.py
+++ task.py
@@ -47,3 +47,12 @@ class Solution:
             if countR == countL:
                 result += 1
         return result      
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        new = ""
+        for i in s:
+            if i.isalnum():
+                new += i.lower()
+        return new == new[::-1]
